b09eebfcda55d2ea4e3e5811d8578518b63af770
DoniDev <abduvokhidovdoniyor4@gmail.com>
2020-06-01 15:21:56 +0500

    user authentication done

--- mask/urls.py
+++ mask/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from django.urls import path
-from django.contrib.auth.models import User
+from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('users/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+
 ]
